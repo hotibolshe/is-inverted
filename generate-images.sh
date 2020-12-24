552c5390cf3a09cf5348038dc60e6c9f3028f7d9
Eugene <Eugene Crystal>
2020-12-24 14:38:50 +0300

    Add half-directory rotation

--- generate-images.sh
+++ generate-images.sh
@@ -1,4 +1,4 @@
-sudo docker run -v /home/eugene/ds/images/test/0:/app/out/ -t belval/trdg:latest trdg -c 1000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 0
-sudo docker run -v /home/eugene/ds/images/test/1:/app/out/ -t belval/trdg:latest trdg -c 1000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 1
-sudo docker run -v /home/eugene/ds/images/test/2:/app/out/ -t belval/trdg:latest trdg -c 1000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 2
-sudo docker run -v /home/eugene/ds/images/test/3:/app/out/ -t belval/trdg:latest trdg -c 1000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 3
+ docker run -v /home/eugene/ds/is-inverted/images/0:/app/out/ -t belval/trdg:latest trdg -c 4000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 0
+ docker run -v /home/eugene/ds/is-inverted/images/1:/app/out/ -t belval/trdg:latest trdg -c 4000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 1
+ docker run -v /home/eugene/ds/is-inverted/images/2:/app/out/ -t belval/trdg:latest trdg -c 4000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 2
+ docker run -v /home/eugene/ds/is-inverted/images/3:/app/out/ -t belval/trdg:latest trdg -c 4000 -rs -num -k 90 -rk -tc '#000000,#FFFFFF' -wd 200 -f 200 -w --length 3 -m 10 -b 3
--- rotate.sh
+++ rotate.sh
@@ -1,5 +1,14 @@
-for szFile in ./*.jpg; 
-do 
-    convert "$szFile" -rotate 180 ./tmp/"$(basename "$szFile")";
+images=(*.jpg)
+total=${#images[@]}
+echo total $total
+count=0
+for image in "${images[@]}"; do 
+    if (($count%2 == 0))
+    then
+         echo count $count
+         convert "$image" -rotate 180 ./tmp/"$(basename "$image")";
+         rm "./$image"
+    fi;
+    count=$(( count + 1  ));
 done
 
